74376c9b56dcca685329368c0694429a8c0a31aa
simonfei123 <tfei@ucsd.edu>
2025-03-09 01:01:37 +0000

    Add CLIP-only reconstruction script

--- scripts-thingseeg2/reconstruct_from_embeddings_clip-only.py
+++ scripts-thingseeg2/reconstruct_from_embeddings_clip-only.py
@@ -14,7 +14,6 @@ args = parser.parse_args()
 sub=int(args.sub)
 
 pred_clip = np.load(f"cache/thingseeg2_preproc/predicted_embeddings/sub-{sub:02d}/regress_clip.npy", mmap_mode='r') # Load the embeddings
-pred_vae = np.load(f"cache/thingseeg2_preproc/predicted_embeddings/sub-{sub:02d}/regress_vae.npy", mmap_mode='r')
 recon_dir = f"results/thingseeg2_preproc/sub-{sub:02d}/unclip/" # Directory to save the reconstructed images
 os.makedirs(recon_dir, exist_ok=True)
 
@@ -30,15 +29,10 @@ extra_portion = torch.cat([torch_ones, torch_zeros])
 
 for i, embedding in enumerate(pred_clip):
     print(i)
-    vae_latent = pred_vae[i].reshape((1, 4, 96, 96))
-    vae_latent = torch.from_numpy(vae_latent).to(device).half()
-    torch.manual_seed(0)
-    noise_latent=torch.randn(vae_latent.shape, device=device).half()
-    vae_latent = vae_latent*0.02 + noise_latent
     embedding = torch.tensor(embedding, device=device, dtype=torch.float16)
     embedding = torch.cat([embedding, extra_portion]).unsqueeze(0)
     negative_prompt_embeds = torch.zeros_like(embedding)
     embedding = torch.cat([negative_prompt_embeds, embedding])
     torch.manual_seed(0)
-    image = pipe.decode(embedding, latents=vae_latent, guidance_scale=7.5).images[0]
+    image = pipe.decode(embedding).images[0]
     image.save(recon_dir + f"{i}.png")
